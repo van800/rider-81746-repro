7a778554bcd64dc90c20535105fac87b49130665
Morrison Cole <morrison.cole@unity3d.com>
2022-09-20 13:11:02 +0900

    feat: demonstrate bug

---
+++ Assets/First/Example.cs
@@ -0,0 +1,12 @@
+namespace First
+{
+    public class Example
+    {
+        readonly string m_CanBeNull;
+        
+        public Example(string canBeNull)
+        {
+            m_CanBeNull = canBeNull;
+        }
+    }
+}
---
+++ Assets/Second/ExampleUsage.cs
@@ -0,0 +1,10 @@
+using First;
+
+namespace Second
+{
+    public class ExampleUsage
+    {
+        // ReSharper disable once UnusedMember.Local
+        readonly Example m_ExampleUsage = new(null);
+    }
+}
